fbfcf5525526d6065c6776351172a45d69dc3cbb
Raphael Fernandes <holyfernandes@yahoo.co.uk>
2021-01-06 19:38:52 -0300

    Implementado herança de dispositivo printer e scanner.

---
+++ ExemploHerancaMultipla/Devices/Scanner.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace ExemploHerancaMultipla.Devices
+{
+    class Scanner : Device
+    {
+        public override void ProcessDoc(string document)
+        {
+            Console.WriteLine("Scanner processing: " + document);
+        }
+
+        public string Scan()
+        {
+            return "Scanner scan result";
+        }
+    }
+}
--- ExemploHerancaMultipla/Program.cs
+++ ExemploHerancaMultipla/Program.cs
@@ -1,12 +1,18 @@
 ﻿using System;
-
+using ExemploHerancaMultipla.Devices;
 namespace ExemploHerancaMultipla
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Printer p = new Printer() { SerialNumber = 1080 };
+            p.ProcessDoc("My Letter");
+            p.Print("My Letter");
+
+            Scanner s = new Scanner() { SerialNumber = 2003 };
+            s.ProcessDoc("My Email");
+            Console.WriteLine(s.Scan());
         }
     }
 }
